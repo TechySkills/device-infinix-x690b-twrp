0b7ff641c75cc0fd63570f2dbc074c7092b3e611
TechySkills <82161063+TechySkills@users.noreply.github.com>
2024-08-30 20:57:40 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_Infinix-X690B.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_Infinix-X690B-user \
-    twrp_Infinix-X690B-userdebug \
-    twrp_Infinix-X690B-eng
+    twrp_X690B-user \
+    twrp_X690B-userdebug \
+    twrp_X690B-eng
