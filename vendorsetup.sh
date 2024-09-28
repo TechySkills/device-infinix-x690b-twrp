9f29fee4ac2bdadb26010ce54f50478a3d7800f4
TechySkills <82161063+TechySkills@users.noreply.github.com>
2024-09-28 22:43:05 +0500

    Changed the export policy from 11.0 to 12.1
    Fixed the version of Ofox in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -25,7 +25,7 @@ fi
 if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		# Version / Maintainer infos
 		export OF_MAINTAINER="TechySkills"
-		export FOX_VERSION=R11.0
+		export FOX_VERSION=R12.1
 		export FOX_BUILD_TYPE="Stable"
 
 		# Device info
@@ -47,7 +47,7 @@ if [ "$1" = "$FDEVICE" ] || [  "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		export OF_QUICK_BACKUP_LIST="/boot;/data;"
  		export FOX_ENABLE_APP_MANAGER=1
 		export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-    	export TW_DEFAULT_LANGUAGE="en"
+    		export TW_DEFAULT_LANGUAGE="en"
 		export OF_USE_SYSTEM_FINGERPRINT=1
  		export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
  		export FOX_DELETE_AROMAFM=1
